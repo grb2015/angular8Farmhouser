4d179c0b9ab5436e8f7dbd749cba0defc9758fc5
guorenbin <XMG3-Rel.1>
2019-08-18 19:54:50 +0800

    go o

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,7 +21,6 @@ const routes: Routes = [
   // { path: '/angular8Farmhouser', component: AboutComponent },
   // { path: '/angular8Farmhouser/angular8Farmhouser/', component: AboutComponent },
   // { path: '/angular8Farmhouser/angular8Farmhouser', component: AboutComponent },
-
   { path: 'header', component: HeaderComponent },
   { path: 'footer', component: FooterComponent },
   { path: 'about', component: AboutComponent },
